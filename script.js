6c0ba6fd785f22cef8b4f6b95bbc1dbe4b4c423a
Natalia <n.laszczkowska@artneo.pl>
2025-02-11 15:46:58 +0100

    Add header for all subpages

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const isHomePage = window.location.pathname.endsWith("index.html") || window.location.pathname === "/";
+
+    if (!isHomePage) {
+        fetch("header.html")
+            .then(response => response.text())
+            .then(data => document.getElementById("header").innerHTML = data);
+    }
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
+});
+
+
 function toggleMenu() {
     const menu = document.querySelector('.nav-links-container');
+    menu.classList.toggle('show');
+    document.body.classList.toggle('no-scroll');
+}
+
+function toggleMenu() {
+    const menu = document.querySelector('.nav-links-cont');
     menu.classList.toggle('show');
     document.body.classList.toggle('no-scroll');
 }
